962d0f74da1a1100293c9dbefb6463a0f4b52d90
Agus Safarudin <42315161+agussafarudin@users.noreply.github.com>
2019-01-05 22:06:35 +0700

    safarudin

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<conio.h>
+#include<stdio.h>
+
+int TAMBAH(int A, int B);
+    int main(){
+        int hasil;
+        hasil=TAMBAH(2,3);
+        printf("hasil=%d\n",hasil);
+        getch();
+        }
+        int TAMBAH(int A,int B){
+            int C;
+            C=A+B;
+            {
+                float C;
+                C=100;;
+            }
+            return(C);
+            }
+
